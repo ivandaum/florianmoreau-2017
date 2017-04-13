8bda58a77d68d3d0d00cc0a00ad0697714c8f145
Ivan Daum <ivan.daum@gmail.com>
2017-04-13 16:19:35 +0200

    feat(analytics): add anaylitcs

--- public/templates/partials/head.php
+++ public/templates/partials/head.php
@@ -20,6 +20,17 @@
 
     <title><?= get_option('blogname') ?></title>
     <link href="<?= PUBLIC_PATH ?>compressed/main.min.css" rel="stylesheet" type="text/css" />
+
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-49671786-1', 'florianmoreau.fr');
+      ga('send', 'pageview');
+    </script>
+
 </head>
 <?php global $bodyClass ?>
 <body class="<?= $bodyClass ?>">
